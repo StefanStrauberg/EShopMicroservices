8bd6c0889de815b7e5f8cf85b05dba1b0b2c6e88
Strauberg <StefanStrauberg@gmail.com>
2024-05-23 16:25:19 +0300

    Adding Get method

---
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,20 @@
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductQuery() : IRequest<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsHandler(IDocumentSession session) : IRequestHandler<GetProductQuery, GetProductsResult>
+{
+  readonly IDocumentSession _session = session
+    ?? throw new ArgumentNullException(nameof(session));
+
+  public async Task<GetProductsResult> Handle(GetProductQuery request, CancellationToken cancellationToken)
+  {
+    // Get all products from the DB
+    var products = await _session.Query<Product>().ToListAsync(cancellationToken);
+
+    // Return GetProductsResult result
+    return new GetProductsResult(products);
+  }
+}
