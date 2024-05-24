9373d790e7db1ab33ab079d4d57ffb09f992f916
Strauberg <StefanStrauberg@gmail.com>
2024-05-24 15:48:26 +0300

    adding the get by category method

---
+++ Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,20 @@
+
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+
+public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+internal class GetProductsByCategoryHandler(IDocumentSession session) : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+  readonly IDocumentSession _session = session
+    ?? throw new ArgumentNullException(nameof(session));
+
+  public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery request, CancellationToken cancellationToken)
+  {
+    // Get product by category from the DB
+    var products = await _session.Query<Product>().Where(x => x.Category.Contains(request.Category)).ToListAsync(token: cancellationToken);
+    // Return GetProductByCategoryResult result
+    return new GetProductsByCategoryResult(products);
+  }
+}
