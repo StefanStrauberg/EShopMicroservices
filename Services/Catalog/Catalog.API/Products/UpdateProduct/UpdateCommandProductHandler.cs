2be61ab36e70aa6263be71a9668ecc42889e6de4
Strauberg <StefanStrauberg@gmail.com>
2024-05-28 10:22:05 +0300

    adding update and delete validator

--- Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateCommandProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateCommandProductHandler.cs
@@ -9,6 +9,20 @@ public record UpdateProductCommand(Guid Id,
 
 public record UpdateProductCommandResult(bool IsSuccess);
 
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+  public UpdateProductCommandValidator()
+  {
+    RuleFor(x => x.Id).NotEmpty()
+                      .WithMessage("Product ID is required");
+    RuleFor(x => x.Name).NotEmpty()
+                        .WithMessage("Name is required")
+                        .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+    RuleFor(x => x.Price).GreaterThan(0)
+                            .WithMessage("Price must be greater than 0");
+  }
+}
+
 internal class UpdateCommandProductHandler(IDocumentSession session, ILogger<UpdateCommandProductHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductCommandResult>
 {
   readonly IDocumentSession _session = session
