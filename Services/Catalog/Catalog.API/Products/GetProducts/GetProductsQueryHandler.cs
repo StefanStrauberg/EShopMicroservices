947f8b1d3bc004f1a0029163f30480e5b88496b2
Strauberg <StefanStrauberg@gmail.com>
2024-05-29 16:19:32 +0300

    Ading pagination function for GetProducts method

--- Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,18 +1,22 @@
+using Marten.Pagination;
+
 namespace Catalog.API.Products.GetProducts;
 
-public record GetProductQuery() : IRequest<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10)
+  : IRequest<GetProductsResult>;
 
 public record GetProductsResult(IEnumerable<Product> Products);
 
-internal class GetProductsQueryHandler(IDocumentSession session) : IRequestHandler<GetProductQuery, GetProductsResult>
+internal class GetProductsQueryHandler(IDocumentSession session)
+  : IRequestHandler<GetProductsQuery, GetProductsResult>
 {
   readonly IDocumentSession _session = session
     ?? throw new ArgumentNullException(nameof(session));
 
-  public async Task<GetProductsResult> Handle(GetProductQuery request, CancellationToken cancellationToken)
+  public async Task<GetProductsResult> Handle(GetProductsQuery request, CancellationToken cancellationToken)
   {
     // Get all products from the DB
-    var products = await _session.Query<Product>().ToListAsync(cancellationToken);
+    var products = await _session.Query<Product>().ToPagedListAsync(request.PageNumber ?? 1, request.PageSize ?? 10, cancellationToken);
 
     // Return GetProductsResult result
     return new GetProductsResult(products);
