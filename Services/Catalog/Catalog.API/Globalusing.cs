01faff96ff6db01fa3c9c1016f20fa7b8f9ac51d
Strauberg <StefanStrauberg@gmail.com>
2024-05-29 17:11:16 +0300

    a little ipgrade health check for API

--- Services/Catalog/Catalog.API/Globalusing.cs
+++ Services/Catalog/Catalog.API/Globalusing.cs
@@ -11,3 +11,5 @@ global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Exceptions.Handler;
 global using Catalog.API.Data;
 global using Marten.Schema;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using HealthChecks.UI.Client;
